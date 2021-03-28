e08edd7fc02a622d8d78325835762898158871e6
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-28 20:02:25 -0300

    add dependency nodemon and create first route

---
+++ src/server.js
@@ -0,0 +1,8 @@
+﻿const express = require("express")
+const server = express()
+
+server.get('/', (request, response) => {
+  return response.send('OLA update')
+})
+
+server.listen(3000, () => console.log('Rodando o server na porta 3000'))
