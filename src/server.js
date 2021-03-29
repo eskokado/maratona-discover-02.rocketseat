4a709626aa46b55c84376769a47f5ef30a78692b
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-29 10:12:31 -0300

    enable static files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 ﻿const express = require("express")
 const server = express()
 
+// habilitar arquivos estáticos
+server.use(express.static("public"))
+
 server.get('/', (request, response) => {
   return response.sendFile(__dirname + "/views/index.html")
 })
