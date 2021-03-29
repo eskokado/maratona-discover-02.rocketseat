2f2e781f6d46eb8ee1437d68b8560dc77fd44dc9
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-29 10:05:38 -0300

    move images, scripts and styles to public

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const server = express()
 
 server.get('/', (request, response) => {
-  return response.send('OLA update')
+  return response.sendFile(__dirname + "/views/index.html")
 })
 
 server.listen(3000, () => console.log('Rodando o server na porta 3000'))
