af3608465be00c3491ecc4fda35f77aeea5c095e
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-31 12:54:19 -0300

    update job

--- src/routes.js
+++ src/routes.js
@@ -72,7 +72,7 @@ const Job = {
           ...job,
           remaining,
           status,
-          budget: Profile.data["value-hour"] * job["total-hours"]
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
         }
       })
       return res.render(views + "index", { jobs: updateJobs })
@@ -95,6 +95,48 @@ const Job = {
 
       return res.redirect('/')
     },
+
+    show (req, res) {
+
+      const jobId = req.params.id
+
+      const job = Job.data.find((job) => Number(job.id) === Number(jobId))
+
+      if (!job) {
+        return res.send('Job not found')
+      }
+
+      job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+      return res.render(views + "job-edit", { job })
+    },
+
+    update (req, res) {
+
+      const jobId = req.params.id
+
+      const job = Job.data.find((job) => Number(job.id) === Number(jobId))
+
+      if (!job) {
+        return res.send('Job not found')
+      }
+
+      const updateJob = {
+        ...job,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+      }
+
+      Job.data = Job.data.map(job => {
+        if (Number(job.id) === Number(jobId)) {
+          job = updateJob
+        }
+        return job
+      })
+
+      return res.redirect('/job/' + jobId)
+    },
   },
 
   services: {
@@ -113,7 +155,8 @@ const Job = {
       const dayInMs = 1000 * 60 * 60 * 24
       const dayDiff = Math.floor(timeDiffInMs / dayInMs)
       return dayDiff
-    }
+    },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
   }
 }
 
@@ -121,7 +164,8 @@ routes.get('/', Job.controllers.index)
 
 routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/job/:id', Job.controllers.show)
+routes.post('/job/:id', Job.controllers.update)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
