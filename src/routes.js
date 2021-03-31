a6fb5d352a0743bee27789bbdd072f26c6fad31f
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-31 13:28:06 -0300

    Delete job

--- src/routes.js
+++ src/routes.js
@@ -137,6 +137,15 @@ const Job = {
 
       return res.redirect('/job/' + jobId)
     },
+
+    delete (req, res) {
+
+      const jobId = req.params.id
+
+      Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+      return res.redirect('/')
+    }
   },
 
   services: {
@@ -166,6 +175,7 @@ routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
